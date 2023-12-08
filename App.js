77d8fa436b2fdb8b68b2effc857baf643445ffd1
Rafi <ramdanirafi412@gmail.com>
2023-12-08 10:38:59 +0700

    feat(navigation): add navigation setup

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import {Onboarding} from './src/screens';
 
+const Stack = createStackNavigator();
+
 const App = () => {
-  return <Onboarding />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
